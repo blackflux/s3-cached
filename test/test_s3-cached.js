774ddef33792d2cbea5bf39c9be7c928393afb3c
blackflux <blackflux@unfine.de>
2017-11-19 12:38:31 -0800

    feat: credentials are now required for input chore: added test for key not found fix: hard coded some credentials with minimal permissions for easy cassette re-recording

--- test/test_s3-cached.js
+++ test/test_s3-cached.js
@@ -2,15 +2,28 @@ const path = require("path");
 const expect = require("chai").expect;
 const nockBack = require('nock').back;
 const s3 = require("./../lib/s3-cached")({
-  bucket: 's3-cached-test-bucket'
+  bucket: 's3-cached-test-bucket',
+  // test key with minimal permissions making it easy to re-generate cassettes
+  access_key_id: "AKIAJYSLHIWEUJLG2TJQ",
+  secret_access_key: "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"
 });
 
 nockBack.setMode('record');
 nockBack.fixtures = path.join(__dirname, "__cassette");
 
 describe("Testing S3-Cached", () => {
+  it("Testing JSON Not Found", (done) => {
+    nockBack(`unknown-file.json_recording.json`, {}, (nockDone) => {
+      s3.getJsonObjectCached("unknown-file.json").catch((err) => {
+        expect(["The specified key does not exist.", "Access Denied"]).to.contain(err.message);
+        nockDone();
+        done();
+      });
+    });
+  });
+
   it("Testing JSON", (done) => {
-    nockBack(`test_json.json_recording.json`, {}, (nockDone) => {
+    nockBack(`large.json_recording.json`, {}, (nockDone) => {
       s3.getJsonObjectCached("large.json").then((json) => {
         expect(json[0].tags).to.include("anim");
         expect(json[1].friends[2].name).to.equal("Susanne Alvarez");
@@ -21,7 +34,7 @@ describe("Testing S3-Cached", () => {
   });
 
   it("Testing GZIP", (done) => {
-    nockBack(`test_gzip.json_recording.json`, {}, (nockDone) => {
+    nockBack(`large.json.gz_recording.json`, {}, (nockDone) => {
       // Important: There is currently a bug in Nock, which requires us to add gzip content type
       // manually into  the recorded files. Reference: https://github.com/node-nock/nock/issues/1001
       s3.getDeflatedObjectCached("large.json.gz").then((r) => {
